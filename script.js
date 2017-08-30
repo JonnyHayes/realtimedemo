df1ca92a8fc0b2fda7acdf9c80f42a23b2f9da63
Christian Nwamba <nwambachristian@gmail.com>
2017-08-30 09:32:28 +0100

    Initial commit with starter files

---
+++ script.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+    $('#realtime').DataTable( {
+        data: dataSet,
+        columns: [
+            { title: "Name" },
+            { title: "Position" },
+            { title: "Office" },
+            { title: "Extn." },
+            { title: "Start date" },
+            { title: "Salary" }
+        ]
+    } );
+} );
