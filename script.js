1488cd407606fc1d41d47b91b3558f37e9f58e01
Jon Hayes <jonnyhayes@gmail.com>
2019-05-09 12:10:33 -1000

    add

--- script.js
+++ script.js
@@ -1,19 +1,20 @@
 const app = {
   buildForm() {
     return [
-      $('#name').val(),
-      $('#position').val(),
-      $('#office').val(),
-      $('#extn').val(),
-      $('#startDate')
+      $("#name").val(),
+      $("#position").val(),
+      $("#office").val(),
+      $("#extn").val(),
+      $("#startDate")
         .val()
-        .replace(new RegExp('-', 'g'), '/'),
-      `$${$('#salary').val()}`
+        .replace(new RegExp("-", "g"), "/"),
+      `$${$("#salary").val()}`
     ];
   },
   sendToServer() {
     const formData = this.buildForm();
-    axios.post('http://localhost:2000/record', formData)
+    axios
+      .post("http://localhost:2000/record", formData)
       .then(response => console.log(response));
   },
   addRow(dataTable, data) {
@@ -22,47 +23,50 @@ const app = {
 
     const addedRowNode = addedRow.node();
     console.log(addedRowNode);
-    $(addedRowNode).addClass('highlight');
+    $(addedRowNode).addClass("highlight");
   },
   selectRow(dataTable) {
-    if ($(this).hasClass('selected')) {
-      $(this).removeClass('selected');
+    if ($(this).hasClass("selected")) {
+      $(this).removeClass("selected");
     } else {
-      dataTable.$('tr.selected').removeClass('selected');
-      $(this).addClass('selected');
+      dataTable.$("tr.selected").removeClass("selected");
+      $(this).addClass("selected");
     }
   },
   removeRow(dataTable) {
-    dataTable.row('.selected').remove().draw( false );
+    dataTable
+      .row(".selected")
+      .remove()
+      .draw(false);
   },
   start() {
-    const dataTable = $('#realtime').DataTable({
+    const dataTable = $("#realtime").DataTable({
       data: dataSet,
       columns: [
-        { title: 'Name' },
-        { title: 'Position' },
-        { title: 'Office' },
-        { title: 'Extn.' },
-        { title: 'Start date' },
-        { title: 'Salary' }
+        { title: "Name" },
+        { title: "Position" },
+        { title: "Office" },
+        { title: "Extn." },
+        { title: "Start date" },
+        { title: "Salary" }
       ]
     });
 
-    $('#add').on('click', this.sendToServer.bind(this));
+    $("#add").on("click", this.sendToServer.bind(this));
     const self = this;
-    $('#realtime tbody').on('click', 'tr', function(){
+    $("#realtime tbody").on("click", "tr", function() {
       self.selectRow.bind(this, dataTable)();
     });
-    $('#remove').on('click', this.removeRow.bind(this, dataTable));
+    $("#remove").on("click", this.removeRow.bind(this, dataTable));
 
     // Pusher
-    var pusher = new Pusher('App Key', {
-      cluster: 'CLUSTER',
+    var pusher = new Pusher("b5262cc4db5c312a3457", {
+      cluster: "us3",
       encrypted: true
     });
 
-    var channel = pusher.subscribe('records');
-    channel.bind('new-record', (data) => {
+    var channel = pusher.subscribe("records");
+    channel.bind("new-record", data => {
       this.addRow(dataTable, data);
     });
   }
--- server.js
+++ server.js
@@ -1,26 +1,26 @@
-const express = require('express');
-const bodyParser = require('body-parser')
-const Pusher = require('pusher')
-const cors = require('cors')
+const express = require("express");
+const bodyParser = require("body-parser");
+const Pusher = require("pusher");
+const cors = require("cors");
 
 const app = express();
-
-app.use(cors())
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
+const http = require("http").Server(app);
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 const pusher = new Pusher({
-  appId: 'APP ID',
-  key: 'APP KEY',
-  secret: 'SECRET',
-  cluster: 'CLUSTER',
+  appId: "777073",
+  key: "b5262cc4db5c312a3457",
+  secret: "afaa867da05677aa54cf",
+  cluster: "us3",
   encrypted: true
 });
 
-app.post('/record', (req, res) => {
+app.post("/record", (req, res) => {
   console.log(req.body);
-  pusher.trigger('records', 'new-record', req.body);
-  res.send('Pushed');
-})
+  pusher.trigger("records", "new-record", req.body);
+  res.send("Pushed");
+});
 
-app.listen(2000, () => console.log('Listening at 2000'));
+http.listen(2000, () => console.log("Listening at 2000"));
